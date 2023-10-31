92785bfc433ac996840a037d63d4c47355965aa9
dennisgon <dennisgon69@hotmail.com>
2023-10-31 10:09:00 +0700

    translation into bahasa

---
+++ src/messages/id/errorapi.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'data_json_error' => 'harus berupa tipe "string" untuk diuraikan kembali.',
+    'data_content_error' => 'error_json harus mengandung kunci pesan dan serverName dengan nilainya.',
+    'mail_create_issue' => 'Buat isu baru di GitHub',
+];
